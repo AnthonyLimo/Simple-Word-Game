1e16064bc6604ab8aec51300f7ff298e7888245d
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-04-23 13:41:52 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final noun = new WordNoun.random();
+    return new MaterialApp(
+      title: 'Word Game',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Word Game'),
+        ),
+        body: new Center(
+          child: new Text(
+              'Noun: '),
+        ),
+      ),
+    );
+  }
+}
+
